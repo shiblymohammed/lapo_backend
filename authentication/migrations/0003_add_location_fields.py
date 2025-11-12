028f7a5bee47989643efd899c8a904577e523912
shibilymohammed <shibilymohammed75@gmail.com>
2025-11-12 16:47:14 +0530

    Fix migration dependency for location fields

--- authentication/migrations/0003_add_location_fields.py
+++ authentication/migrations/0003_add_location_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0002_alter_customuser_firebase_uid_and_more'),
+        ('authentication', '0002_alter_customuser_phone_number'),
     ]
 
     operations = [
